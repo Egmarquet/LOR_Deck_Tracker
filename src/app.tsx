c3ec1bc374ba0611ea826bdbdb242ab9584ee59e
egmarquet <egmarquet@gmail.com>
2020-03-10 14:01:19 -0400

    Initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,83 @@
 import { Text, Window, hot, View } from "@nodegui/react-nodegui";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useInterval, isEmpty } from "./components/tools"
 import { QIcon } from "@nodegui/nodegui";
-import { StepOne } from "./components/stepone";
-import { StepTwo } from "./components/steptwo";
 import nodeguiIcon from "../assets/nodegui.jpg";
+const axios = require('axios');
 
-const minSize = { width: 500, height: 520 };
+const minSize = { width: 500, height: 800 };
+const port = 21337;
 const winIcon = new QIcon(nodeguiIcon);
-class App extends React.Component {
-  render() {
-    return (
-      <Window
-        windowIcon={winIcon}
-        windowTitle="Hello 👋🏽"
-        minSize={minSize}
-        styleSheet={styleSheet}
-      >
-        <View style={containerStyle}>
-          <Text id="welcome-text">Welcome to NodeGui 🐕</Text>
-          <Text id="step-1">1. Play around</Text>
-          <StepOne />
-          <Text id="step-2">2. Debug</Text>
-          <StepTwo />
-        </View>
-      </Window>
-    );
+
+/*
+appState:
+  "waiting" (0): waiting for a deck to be played based on gamestate
+  "active" (1): actively tracking the deck being played
+
+initialDeckList:
+  The initial deck list found by requesting from http://127.0.0.1:21337/static-decklist
+  (chaning this only from waiting and do )
+
+currDeckList:
+  The current state of the deck list
+*/
+function App(){
+  const [deckActive, setDeckActive] = useState(false);
+  const [errState, setErrorState] = useState({});
+
+  const [initialDeckList, setInitialDeckList] = useState({DeckCode:null, CardsInDeck:null});
+  const [currDeckList, setCurrDeckList] = useState({DeckCode:null, CardsInDeck:null});
+
+  // Server polling circuit
+  useInterval(() => {
+    if (!deckActive){
+      getInitialState();
+    }
+    else{
+      updateDeckState();
+    }
+  }, 1500);
+
+  // Setting initial deck state if a deck is detected
+  const getInitialState = () => {
+    axios.get('http://127.0.0.1:21337/static-decklist')
+      .then(function (response){
+        if ((response.data.DeckCode) != null && !isEmpty(response.data.CardsInDeck)){
+          console.log("deck detected")
+          setInitialDeckList(response.data)
+          setCurrDeckList(response.data)
+          setDeckActive(true)
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
   }
+
+  // Updating deck states
+  function updateDeckState(){
+    console.log("Deck Detected")
+    console.log(initialDeckList)
+  }
+
+  return (
+    <Window
+      windowIcon={winIcon}
+      windowTitle="Hello 👋🏽"
+      minSize={minSize}
+      styleSheet={styleSheet}
+    >
+      <View style={containerStyle}>
+        <Text id="welcome-text">Welcome to NodeGui 🐕</Text>
+        <Text id="welcome-text">Hello World!</Text>
+      </View>
+    </Window>
+  );
 }
 
 const containerStyle = `
-  flex: 1; 
+  flex: 1;
 `;
 
 const styleSheet = `
@@ -38,13 +86,6 @@ const styleSheet = `
     padding-top: 20px;
     qproperty-alignment: 'AlignHCenter';
     font-family: 'sans-serif';
-  }
-
-  #step-1, #step-2 {
-    font-size: 18px;
-    padding-top: 10px;
-    padding-horizontal: 20px;
-  }
-`;
+  }`;
 
 export default hot(App);
